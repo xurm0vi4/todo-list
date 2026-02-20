458de0f3beba44f0f38b0af6c139023e275f584a
xurm0vi4 <zarsasho@gmail.com>
2026-02-20 13:18:13 +0200

    feat: add global styles and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import './App.css'
+import './App.scss'
 
 function App() {
-  return <div>Init</div>
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1 className="app-title">Todo Board</h1>
+      </header>
+      <main className="app-main"></main>
+    </div>
+  )
 }
 
 export default App
